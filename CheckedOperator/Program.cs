d81c766d1446b3d1b8848a0c68f8089c86407baa
ali ahmadi <ahmadiali709@hotmail.com>
2022-01-13 23:44:07 -0800

    Add project files.

---
+++ CheckedOperator/Program.cs
@@ -0,0 +1,13 @@
+﻿try
+{
+    int maxValue = int.MaxValue;
+
+    int minValue = maxValue + 1; // OUTPUT: int.MinValue
+    Console.WriteLine(minValue);
+
+    var b = checked(maxValue + 1); // OUTPUT: System.OverflowException: Arithmetic operation resulted in an overflow.
+}
+catch (Exception)
+{
+    //handle exception
+}
